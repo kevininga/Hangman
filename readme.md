c896e721a5264d53b0fe467f3c1cfe14e584957e
kevininga-ga <kevininga813@gmail.com>
2023-05-12 11:03:37 -0400

    final hangman project

---
+++ readme.md
@@ -0,0 +1,21 @@
+Project-1 Game
+HANGMAN
+
+Project Description
+Welcome to Hangman, a game where you  have to guess ta random word. Players select one of three different categories (countries, foods, or jobs) from the pop up screen and a random word will be generated from that category. The player must then try to guess the word in 6 guesses or under in order to score a point, if not the player loses the game! This was developed using HTML, CSS, and Javascript.
+
+Wire Frames
+
+
+User Stories
+MVP Goals
+
+As the player, you will be able to click the letters on the screen and try to guess the word
+As the computer, it will tell the player if they got the guess correctly or not
+As the computer, it will notify the player if they guessed the letter correctly
+As the player, you will be able to reset the game at anytime to get a clean slate and restart the game.
+As the player, you have 6 tries to try to guess the word before losing
+Stretch Goals
+
+Integrate it with AI so it can help me pick difficult and more obscure words to guess to add a difficulty setting
+Some fun/cool animations for all gameplay.
---
+++ script.js
@@ -0,0 +1,261 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+const countries = [
+  "ALBANIA", "ALGERIA", "BERMUDA", "CANADA", "CHILE", "DENMARK", "ENGLAND", "ECUADOR", "FIJI",  "GERMANY", "HAITI", "MADAGASCAR", 
+  // Add more countries here
+];
+
+const food = [
+  "ANCHOVY", "BAGEL", "BURRITO", "CURRY", "DRAGONFRUIT", "HAMBURGER", "LASAGNA", "PIE", "PIZZA", "QUICHE", "RICE", "SALAD"
+  // Add more food items here
+];
+
+const jobs = [
+  "ARCHITECT", "ANIMATOR", "BARISTA", "DOCTOR", "DANCER", "ENGINEER", "TEACHER", "CHEF", "TEACHER", "FIREFIGHTER", "JUDGE", "ORTHODONTIST", "PILOT", "PLUMBER", 
+  // Add more job titles here
+];
+
+const maxWrong = 6;
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let country = '';
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;
+let hintRevealed = false;
+let score = 0;
+let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+let category = countries; // Default category
+
+function selectCategory() {
+  const categoryModal = document.getElementById("categoryModal");
+  categoryModal.style.display = "block";
+
+  const categoryButtons = document.getElementsByClassName("category-button");
+  for (let i = 0; i < categoryButtons.length; i++) {
+    categoryButtons[i].addEventListener("click", function () {
+      const selectedCategory = this.getAttribute("data-category");
+      setCategory(selectedCategory);
+      categoryModal.style.display = "none";
+    });
+  }
+}
+
+function setCategory(selectedCategory) {
+  switch (selectedCategory) {
+    case "countries":
+      category = countries;
+      break;
+    case "food":
+      category = food;
+      break;
+    case "jobs":
+      category = jobs;
+      break;
+    default:
+      alert("Invalid category");
+  }
+  reset();
+}
+
+// Drawing functions
+function drawInitialGameBoard() {
+  ctx.lineWidth = "6";
+  ctx.strokeStyle = "black";
+  ctx.beginPath();
+  ctx.rect(310, 290, 180, 1);
+  ctx.rect(400, 10, 1, 280);
+  ctx.rect(240, 10, 160, 1);
+  ctx.rect(240, 10, 1, 30);
+  ctx.stroke();
+}
+
+function drawBodyPart() {
+  ctx.beginPath();
+  switch (mistakes) {
+    case 1: ctx.arc(240, 70, 30, 0, Math.PI * 2, true); // Head
+      break;
+    case 2: ctx.moveTo(240, 100); ctx.lineTo(240, 220); // Body
+      break;
+    case 3: ctx.moveTo(240, 140); ctx.lineTo(200, 200); // Left Arm
+      break;
+    case 4: ctx.moveTo(240, 140); ctx.lineTo(280, 200); // Right Arm
+      break;
+    case 5: ctx.moveTo(240, 215); ctx.lineTo(200, 280); // Left Leg
+      break;
+    case 6: ctx.moveTo(240, 215); ctx.lineTo(280, 280); // Right Leg
+      break;
+    default: break;
+  }
+  ctx.stroke();
+}
+
+// Game initialization functions
+function randomWordFromCategory() {
+  country = category[Math.floor(Math.random() * category.length)];
+}
+
+function generateButtons() {
+  let buttonsHTML = letters.split('').map(letter => `<button class="letter-button" id='${letter}' onClick="processGuessedLetter('${letter}')">${letter}</button>`).join('');
+  let buttonsContainer = `<div class="keyboard">${buttonsHTML}</div>`;
+  document.getElementById('message').innerHTML = buttonsContainer;
+}
+
+// Game play functions
+function processKeyPress(event) {
+  let letter = event.key.toUpperCase();
+  if (/^[A-Z]$/.test(letter)) {
+    processGuessedLetter(letter);
+  }
+}
+
+function processGuessedLetter(chosenLetter) {
+  if (guessed.includes(chosenLetter)) return;
+  guessed.push(chosenLetter);
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+  if (country.includes(chosenLetter)) {
+    updateWordStatus();
+    checkIfGameWon();
+  } else {
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    drawBodyPart();
+  }
+  updateGuessedLetters();
+}
+
+function updateGuessedLetters() {
+  const guessedLettersHTML = guessed.map(letter => `<span>${letter}</span>`).join(' ');
+  document.getElementById('guessedLetters').innerHTML = guessedLettersHTML;
+}
+
+function updateWordStatus() {
+  wordStatus = country.split('').map(letter => {
+    if (guessed.includes(letter)) {
+      return letter;
+    } else if (letter === ' ') {
+      return '   ';
+    } else if (letter === '-') {
+      return '-';
+    } else {
+      return ' _ ';
+    }
+  }).join('');
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function checkIfGameWon() {
+  if (wordStatus === country) {
+    score++; // Increase the score
+    document.getElementById('score').innerHTML = score; // Update the score display
+    document.getElementById('message').innerHTML = 'You Won!!!';
+  }
+}
+
+function checkIfGameLost() {
+  if (mistakes !== maxWrong) return;
+  document.getElementById('wordSpotlight').innerHTML = `The answer was: ${country}`;
+  document.getElementById('message').innerHTML = 'You failed to guess the word in time. Better luck next time!';
+  drawBodyPart(); // Draw the right leg before showing the prompt
+  setTimeout(() => {
+    let playerName = prompt("Please enter your name for the high score table:");
+    highScores.push({ name: playerName.toUpperCase(), score: score });
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(10); // Only keep the top 10
+    localStorage.setItem('highScores', JSON.stringify(highScores)); // Store the scores
+    displayHighScores();
+    score = 0; // Reset the score
+    document.getElementById('score').innerHTML = score; // Update the score display
+  }, 250); // Delay the prompt by .5 seconds (500 milliseconds)
+}
+
+// Display high scores function
+function displayHighScores() {
+  const highScoresList = highScores
+    .map((score, i) => `<tr><td>${i + 1}</td><td>${score.name}</td><td>${score.score}</td></tr>`).join('');
+  document.getElementById('highScores').querySelector('tbody').innerHTML = highScoresList;
+  // Get the modal
+  const modal = document.getElementById("highScoresModal");
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
+  // Show the modal
+  modal.style.display = "block";
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
+function updateMistakes() {
+  document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function revealHint() {
+  if (hintRevealed) return;
+  let hintIndex;
+  const countryLetters = country.split('');
+  do {
+    hintIndex = Math.floor(Math.random() * countryLetters.length);
+  } while (guessed.includes(countryLetters[hintIndex]) || countryLetters[hintIndex] === '-' || countryLetters[hintIndex] === ' ');
+  guessed.push(countryLetters[hintIndex]); // Add the hint letter to the guessed array
+  updateWordStatus(); // Update the word status
+  checkIfGameWon(); // Check if the game is won
+  hintRevealed = true;
+  document.getElementById('hint').setAttribute('disabled', true);
+}
+
+
+// Reset Function
+function reset() {
+  selectCategory(); 
+  mistakes = 0;
+  guessed = [];
+  hintRevealed = false;
+  randomWord();
+  updateWordStatus();
+  updateMistakes();
+  generateButtons();
+  document.getElementById('hint').removeAttribute('disabled');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawInitialGameBoard();
+}
+
+// Initialization
+function reset() {
+  selectCategory();
+  mistakes = 0;
+  guessed = [];
+  hintRevealed = false;
+  randomWordFromCategory();
+  updateWordStatus();
+  updateMistakes();
+  generateButtons();
+  document.getElementById('hint').removeAttribute('disabled');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawInitialGameBoard();
+}
+
+
+// Initialization
+document.addEventListener('DOMContentLoaded', function () {
+  drawInitialGameBoard();
+  randomWordFromCategory();
+  generateButtons();
+  updateWordStatus();
+  document.getElementById('maxWrong').innerHTML = maxWrong;
+  document.getElementById('score').innerHTML = score; // Initialize score display
+
+  // Event Listeners
+  selectCategory();
+  document.addEventListener('keypress', processKeyPress);
+  document.getElementById('hint').addEventListener('click', revealHint);
+  document.getElementById('reset').addEventListener('click', reset);
+});
